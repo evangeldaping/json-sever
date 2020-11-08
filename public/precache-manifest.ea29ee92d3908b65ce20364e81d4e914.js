01c23b7275e4c24fbf2927f3c242a9602eb35d54
evangel <evangeldaping@gmail.com>
2020-11-08 01:34:16 -0800

    for react and angular projects

---
+++ public/precache-manifest.ea29ee92d3908b65ce20364e81d4e914.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa4fcf537a069cab3677903604405e78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5c9145800c90b9dcfac",
+    "url": "/static/css/2.4173b2d3.chunk.css"
+  },
+  {
+    "revision": "e9db54a7e8705956138a",
+    "url": "/static/css/main.95a77cd4.chunk.css"
+  },
+  {
+    "revision": "d5c9145800c90b9dcfac",
+    "url": "/static/js/2.e25e84df.chunk.js"
+  },
+  {
+    "revision": "1d9ed9265a32ea4776472f445cf2df78",
+    "url": "/static/js/2.e25e84df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9db54a7e8705956138a",
+    "url": "/static/js/main.5cc65f31.chunk.js"
+  },
+  {
+    "revision": "aff20233fc33123ab008",
+    "url": "/static/js/runtime-main.74f98a6f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
